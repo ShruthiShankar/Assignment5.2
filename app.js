c7707e7dd766dcdecc5e447abb16af521944812d
shruthishankar <shruthi15shankar@gmail.com>
2017-04-27 22:31:59 +0530

    Second JS element

---
+++ app.js
@@ -0,0 +1,14 @@
+var info ={
+	Name : 'shruthi',
+	Age  : 24,
+	"Date Of Birth" : "15-12-1992",
+	"Place Of Birth" : "Shimoga"
+};
+
+console.log("Name : " + info.Name);
+
+console.log("Age : " + info['Age']);
+
+console.log("Date Of Birth : " + info["Date Of Birth"]);
+
+console.log("Place Of Birth : " + info["Place Of Birth"]);
